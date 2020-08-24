34958ca4a0a4f79240b77231232da25c97acb7c4
Abhinav-Mingwal <abhinav.mingwal@gmail.com>
2020-08-25 00:05:35 +0530

    added build

---
+++ build/precache-manifest.9d4e930cd0c192ceb7e759c363243108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f939f20ba4f7060c52726751489754c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ddd463c9fe94b308790",
+    "url": "/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "323a70bef911abef45fd",
+    "url": "/static/js/2.7a41ffc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a41ffc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ddd463c9fe94b308790",
+    "url": "/static/js/main.73213347.chunk.js"
+  },
+  {
+    "revision": "7382e10e1b2b74f5e1a9",
+    "url": "/static/js/runtime-main.f93c3ea3.js"
+  }
+]);
